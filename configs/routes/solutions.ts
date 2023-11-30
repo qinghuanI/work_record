cbf19c4520ee929bb4d092c5ad5f7822e642dbab
weigaosS <wei.gao@china.talkdesk.com>
2023-11-30 15:12:13 +0800

    调整解决方案

--- configs/routes/solutions.ts
+++ configs/routes/solutions.ts
@@ -1,24 +1,14 @@
 export const solutions = [
   {
-    text: "大数据量渲染",
-    items: [
-      {
-        text: "往上滚动到顶分页加载",
-        link: "/solutions/scroll_to_top_load.html",
-      },
-      {
-        text: "虚拟列表",
-        link: "/solutions/virtual_list.html",
-      },
-    ],
+    text: "虚拟列表",
+    link: "/solutions/virtual_list.html",
   },
   {
-    text: "分片上传",
-    items: [
-      {
-        text: "大文件分片上传",
-        link: "/solutions/multi_part_upload.html",
-      },
-    ],
+    text: "大文件分片上传",
+    link: "/solutions/multi_part_upload.html",
+  },
+  {
+    text: "一种接入GraphQL解决方案",
+    link: "/solutions/graphQL",
   },
 ];
