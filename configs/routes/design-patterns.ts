793e3736f641d9ef1c29b6f4af1f69fbf6e751a3
qinghuanI <1062217965@qq.com>
2023-12-22 21:43:59 +0800

    补充设计模式

--- configs/routes/design-patterns.ts
+++ configs/routes/design-patterns.ts
@@ -10,6 +10,14 @@ export const designPatterns = [
         text: "发布订阅模式",
         link: "/design-patterns/pub-sub",
       },
+      {
+        text: "观察者模式",
+        link: "/design-patterns/observer",
+      },
+      {
+        text: "中介者模式",
+        link: "/design-patterns/mediator",
+      },
     ],
   },
   {
@@ -35,6 +43,14 @@ export const designPatterns = [
       {
         text: "代理模式",
         link: "/design-patterns/proxy",
+      },
+      {
+        text: "桥接模式",
+        link: "/design-patterns/bridge",
+      },
+      {
+        text: "享元模式",
+        link: "/design-patterns/flyweight",
       },
     ],
   },
